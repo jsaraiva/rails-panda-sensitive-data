956ca4cbe0ba5e8e8e87fbc7a17ecc271bf0033b
João Saraiva <joao.saraiva@definescope.com>
2022-03-24 03:24:16 +0000

    no message

--- lib/definerails_sensitivedata/models/sensitive_data_object.rb
+++ lib/definerails_sensitivedata/models/sensitive_data_object.rb
@@ -12,26 +12,34 @@ module DefineRails
           end
 
           def set_encryption_key
-            if self.has_attribute? :encryption_key
-              # 8 chars
-              self.encryption_key =
-                SecureRandom.hex(4) unless self.encryption_key.present?
+            if has_attribute? :encryption_key
+              _encryption_key = encryption_key
+              if _encryption_key.nil? || _encryption_key == ''
+                # 8 chars
+                self.encryption_key = SecureRandom.hex(4)
+              end
             end
           end
 
           def sensitive_data_encryption_key
             set_encryption_key
             the_key =
-              self.encryption_key +
+              encryption_key +
               ::DefineRails::SensitiveData.application_sensitive_data_encryption_key
           end
 
           class_methods do
 
             def add_encrypted_attribute(attribute_name, opts = {})
-              nil_value_visible = true if opts.delete(:nil_value_visible)
-              empty_value_visible = true if opts.delete(:empty_value_visible)
-              treat_nil_as_empty_value = true unless opts.delete(:treat_nil_as_empty_value) == false
+              treat_nil_as_empty_value =
+                opts.delete(:treat_nil_as_empty_value) ? true : false
+
+              empty_value_visible_in_db =
+                opts.delete(:empty_value_visible_in_db) ? true : false
+
+              nil_value_visible_in_db =
+                (opts.delete(:nil_value_visible_in_db) ? true : false) ||
+                (treat_nil_as_empty_value && empty_value_visible_in_db)
 
               attr_encryptor(
                 attribute_name,
@@ -53,57 +61,58 @@ module DefineRails
                   if opts[:attribute]
                     opts[:attribute]
                   else
-                    [
-                      (opts[:prefix] || 'encrypted_'),
-                      attribute_name,
-                      (opts[:suffix] || '')
-                    ].join
+                    prefix = opts[:prefix] || 'encrypted_'
+                    suffix = opts[:suffix] || ''
+
+                    [ prefix, attribute_name, suffix ].join
                   end
                 ).to_sym
 
               define_method attribute_name do
-                mem_value = instance_variable_get(mem_value_attr)
-                return mem_value if mem_value
+                unless value = instance_variable_get(mem_value_attr)
 
-                db_value = send(encrypted_attribute_name)
+                  db_value = send(encrypted_attribute_name)
 
-                if nil_value_visible && db_value.nil?
-                  return '' if treat_nil_as_empty_value
-                  return nil
+                  if nil_value_visible_in_db && db_value.nil?
+                    return '' if treat_nil_as_empty_value
+                    return nil
+                  end
+
+                  if empty_value_visible_in_db && db_value == ''
+                    return ''
+                  end
+
+                  value = send(encryptor_getter_method_name)
                 end
 
-                if empty_value_visible && db_value == ''
-                  return ''
-                end
-
-                send(encryptor_getter_method_name)
+                return '' if treat_nil_as_empty_value && value.nil?
+                return value
               end
 
               define_method "#{ attribute_name }=" do |the_value|
                 encrypted_attribute_name__set = "#{encrypted_attribute_name}="
                 encrypted_attribute_iv_name__set = "#{encrypted_attribute_name}_iv="
 
-                if nil_value_visible && the_value.nil?
-
-                  if treat_nil_as_empty_value
-                    send(encrypted_attribute_name__set, '')
-                    send(encrypted_attribute_iv_name__set, '')
-                    instance_variable_set(mem_value_attr, '')
-                    return ''
-                  end
-
+                if nil_value_visible_in_db &&
+                    the_value.nil? && !treat_nil_as_empty_value
                   send(encrypted_attribute_name__set, nil)
                   send(encrypted_attribute_iv_name__set, nil)
                   instance_variable_set(mem_value_attr, nil)
                   return nil
                 end
 
-                if empty_value_visible && the_value == ''
+                if empty_value_visible_in_db &&
+                    (
+                      the_value == '' ||
+                      (treat_nil_as_empty_value && the_value.nil?)
+                    )
                   send(encrypted_attribute_name__set, '')
                   send(encrypted_attribute_iv_name__set, '')
                   instance_variable_set(mem_value_attr, '')
                   return ''
                 end
+
+                the_value = '' if treat_nil_as_empty_value && the_value.nil?
 
                 send(encryptor_setter_method_name, the_value)
               end
@@ -122,21 +131,47 @@ module DefineRails
                 )
               end
 
+              mem_value_attr = "@#{ in_attribute }"
+
+              encrypted_in_attribute_name =
+                (
+                  if opts[:attribute]
+                    opts[:attribute]
+                  else
+                    prefix = opts[:prefix] || 'encrypted_'
+                    suffix = opts[:suffix] || ''
+
+                    [ prefix, in_attribute, suffix ].join
+                  end
+                ).to_sym
+
               define_method attribute_name do
-                self.send(in_attribute)&.dig(attribute_name)
+                unless the_hash = instance_variable_get(mem_value_attr)
+                  db_value = send(encrypted_in_attribute_name)
+                  return nil if db_value.nil? || db_value == ''
+
+                  the_hash = send(in_attribute)
+                end
+                return nil if the_hash.nil? || the_hash == ''
+
+                the_hash.dig(attribute_name)
               end
 
               define_method "#{ attribute_name }=" do |the_value|
-                the_attribute_value = self.send(in_attribute) || {}
+                unless the_hash = instance_variable_get(mem_value_attr)
+                  db_value = send(encrypted_in_attribute_name)
+                  the_hash =
+                    send(in_attribute) unless db_value.nil? || db_value == ''
+                end
+                the_hash = {} if the_hash.nil? || the_hash == ''
 
                 if the_value.nil?
-                  the_attribute_value.delete attribute_name
+                  the_hash.delete attribute_name
                 else
-                  the_attribute_value[attribute_name] = the_value
+                  the_hash[attribute_name] = the_value
                 end
 
-                self.send "#{in_attribute}=", the_attribute_value
-                # self.send "#{in_attribute}_will_change!"
+                send("#{in_attribute}=", the_hash)
               end
             end
           end
@@ -154,24 +189,32 @@ module DefineRails
           end
 
           def set_encryption_key
-            # 8 chars
-            self.encryption_key =
-              SecureRandom.hex(4) unless self.encryption_key.present?
+            _encryption_key = encryption_key
+            if _encryption_key.nil? || _encryption_key == ''
+              # 8 chars
+              self.encryption_key = SecureRandom.hex(4)
+            end
           end
 
           def sensitive_data_encryption_key
             set_encryption_key
             the_key =
-              self.encryption_key +
+              encryption_key +
               ::DefineRails::SensitiveData.application_sensitive_data_encryption_key
           end
 
           class_methods do
 
             def add_encrypted_attribute(attribute_name, opts = {})
-              nil_value_visible = true if opts.delete(:nil_value_visible)
-              empty_value_visible = true if opts.delete(:empty_value_visible)
-              treat_nil_as_empty_value = true unless opts.delete(:treat_nil_as_empty_value) == false
+              treat_nil_as_empty_value =
+                opts.delete(:treat_nil_as_empty_value) ? true : false
+
+              empty_value_visible_in_db =
+                opts.delete(:empty_value_visible_in_db) ? true : false
+
+              nil_value_visible_in_db =
+                (opts.delete(:nil_value_visible_in_db) ? true : false) ||
+                (treat_nil_as_empty_value && empty_value_visible_in_db)
 
               prefix = opts[:prefix] || 'encrypted_'
               suffix = opts[:suffix] || ''
@@ -182,14 +225,16 @@ module DefineRails
 
               field_name = opts[:attribute] || [ prefix, attribute_name, suffix ].join
 
-              self.send :field, db_attribute_name,
+              send(:field, db_attribute_name,
                 as: field_name,
                 type: String
-              self.send :field, "#{db_attribute_name}_iv",
+              )
+              send(:field, "#{db_attribute_name}_iv",
                 as: "#{field_name}_iv",
                 type: String
+              )
 
-              self.send(:attr_encryptor, attribute_name,
+              send(:attr_encryptor, attribute_name,
                 key: :sensitive_data_encryption_key,
                 marshal: true,
                 encode: true,
@@ -205,48 +250,50 @@ module DefineRails
               alias_method encryptor_setter_method_name, "#{ attribute_name }="
 
               define_method attribute_name do
-                mem_value = instance_variable_get(mem_value_attr)
-                return mem_value if mem_value
+                unless value = instance_variable_get(mem_value_attr)
 
-                db_value = send(field_name)
+                  db_value = send(encrypted_attribute_name)
 
-                if nil_value_visible && db_value.nil?
-                  return '' if treat_nil_as_empty_value
-                  return nil
+                  if nil_value_visible_in_db && db_value.nil?
+                    return '' if treat_nil_as_empty_value
+                    return nil
+                  end
+
+                  if empty_value_visible_in_db && db_value == ''
+                    return ''
+                  end
+
+                  value = send(encryptor_getter_method_name)
                 end
 
-                if empty_value_visible && db_value == ''
-                  return ''
-                end
-
-                send(encryptor_getter_method_name)
+                return '' if treat_nil_as_empty_value && value.nil?
+                return value
               end
 
               define_method "#{ attribute_name }=" do |the_value|
                 encrypted_attribute_name__set = "#{field_name}="
                 encrypted_attribute_iv_name__set = "#{field_name}_iv="
 
-                if nil_value_visible && the_value.nil?
-
-                  if treat_nil_as_empty_value
-                    send(encrypted_attribute_name__set, '')
-                    send(encrypted_attribute_iv_name__set, '')
-                    instance_variable_set(mem_value_attr, '')
-                    return ''
-                  end
-
+                if nil_value_visible_in_db &&
+                    the_value.nil? && !treat_nil_as_empty_value
                   send(encrypted_attribute_name__set, nil)
                   send(encrypted_attribute_iv_name__set, nil)
                   instance_variable_set(mem_value_attr, nil)
                   return nil
                 end
 
-                if empty_value_visible && the_value == ''
+                if empty_value_visible_in_db &&
+                    (
+                      the_value == '' ||
+                      (treat_nil_as_empty_value && the_value.nil?)
+                    )
                   send(encrypted_attribute_name__set, '')
                   send(encrypted_attribute_iv_name__set, '')
                   instance_variable_set(mem_value_attr, '')
                   return ''
                 end
+
+                the_value = '' if treat_nil_as_empty_value && the_value.nil?
 
                 send(encryptor_setter_method_name, the_value)
               end
@@ -254,40 +301,55 @@ module DefineRails
 
             def add_sensitive_attribute_accessors(attribute_name, opts = {})
               in_attribute = opts.delete(:in) || :sensitive_data
-              db_in_attribute = opts.delete(:in_db) || in_attribute
+              in_field_name = opts[:attribute] || opts.delete(:in_db) || [ prefix, in_attribute, suffix ].join
 
-              unless self.respond_to? :sensitive_data
-                self.send :field, "encrypted_#{db_in_attribute}",
-                  as: "encrypted_#{in_attribute}",
+              unless respond_to? in_attribute
+                send(:field, in_field_name,
                   type: String
-                self.send :field, "encrypted_#{db_in_attribute}_iv",
-                  as: "encrypted_#{in_attribute}_iv",
+                )
+                send(:field, "#{in_field_name}_iv",
                   type: String
+                )
 
-                self.send(:attr_encryptor, in_attribute,
+                send(:attr_encryptor, in_attribute,
                   key: :sensitive_data_encryption_key,
                   marshal: true,
                   encode: true,
                   allow_empty_value: true,
+                  attribute: in_field_name,
                   **opts
                 )
               end
 
-              self.send(:define_method, attribute_name) do
-                self.send(in_attribute)&.dig(attribute_name)
+              mem_value_attr = "@#{ in_attribute }"
+
+              send(:define_method, attribute_name) do
+                unless the_hash = instance_variable_get(mem_value_attr)
+                  db_value = send(in_field_name)
+                  return nil if db_value.nil? || db_value == ''
+
+                  the_hash = send(in_attribute)
+                end
+                return nil if the_hash.nil? || the_hash == ''
+
+                the_hash.dig(attribute_name)
               end
 
-              self.send(:define_method, "#{ attribute_name }=") do |the_value|
-                the_attribute_value = self.send(in_attribute) || {}
+              send(:define_method, "#{ attribute_name }=") do |the_value|
+                unless the_hash = instance_variable_get(mem_value_attr)
+                  db_value = send(in_field_name)
+                  the_hash =
+                    send(in_attribute) unless db_value.nil? || db_value == ''
+                end
+                the_hash = {} if the_hash.nil? || the_hash == ''
 
                 if the_value.nil?
-                  the_attribute_value.delete attribute_name
+                  the_hash.delete attribute_name
                 else
-                  the_attribute_value[attribute_name] = the_value
+                  the_hash[attribute_name] = the_value
                 end
 
-                self.send "#{in_attribute}=", the_attribute_value
-                # self.send "#{in_attribute}_will_change!"
+                send("#{in_attribute}=", the_hash)
               end
             end
           end

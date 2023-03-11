e0f074d884465ece4b43984a6845c1c2e8048dbc
João Saraiva <joao.saraiva@definescope.com>
2023-03-11 22:36:45 +0000

    no message

--- lib/definerails_sensitivedata.rb
+++ lib/definerails_sensitivedata.rb
@@ -1,7 +1,8 @@
 require "attr_encrypted"
 
 require "definerails_sensitivedata/sensitivedata"
-require "definerails_sensitivedata/models/sensitive_data_object"
+require "definerails_sensitivedata/models/sensitive_data_object__active_record"
+require "definerails_sensitivedata/models/sensitive_data_object__mongoid"
 
 module DefineRails
   module SensitiveData

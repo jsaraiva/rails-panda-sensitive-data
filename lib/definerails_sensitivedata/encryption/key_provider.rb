86b25a4f2dff1d876c932cd8714674c545f5a1e8
João Saraiva <joao.saraiva@definescope.com>
2023-10-08 20:37:58 +0100

    no message

--- lib/definerails_sensitivedata/encryption/key_provider.rb
+++ lib/definerails_sensitivedata/encryption/key_provider.rb
@@ -2,16 +2,7 @@ module DefineRails
   module SensitiveData
     module Encryption
 
-      class KeyProvider < ::ActiveRecord::Encryption::KeyProvider
-
-        def initialize
-          super(
-            Array(::ActiveRecord::Encryption.config.primary_key)
-              .collect do |password|
-                ::DefineRails::SensitiveData::Encryption::Key.derive_from(password)
-              end
-          )
-        end
+      class KeyProvider
 
         def encryption_key
           random__key_salt = ::SecureRandom.alphanumeric(8)
@@ -23,7 +14,7 @@ module DefineRails
         end
 
         def decryption_keys(encrypted_message)
-          if keys = super(encrypted_message)
+          if keys = primary_key_provider.decryption_keys(encrypted_message)
             if key_salt = encrypted_message.headers[:ks]
               return keys.collect do |k|
                 k.with_salt(key_salt)
@@ -36,7 +27,17 @@ module DefineRails
         private
 
         def active_primary_key
-          @active_primary_key ||= @keys.last
+          @active_primary_key ||= primary_key_provider.encryption_key
+        end
+
+        def primary_key_provider
+          @primary_key_provider ||=
+          ::ActiveRecord::Encryption::KeyProvider.new(
+            Array(::ActiveRecord::Encryption.config.primary_key)
+              .collect do |password|
+                ::DefineRails::SensitiveData::Encryption::Key.derive_from(password)
+              end
+          )
         end
 
       end
--- lib/definerails_sensitivedata/version.rb
+++ lib/definerails_sensitivedata/version.rb
@@ -1,5 +1,5 @@
 module DefineRails
   module SensitiveData
-    VERSION = "2.0.1"
+    VERSION = "2.0.2"
   end
 end
